bd28cdd186057f42457b877d8897c4f0c9383c2f
Peter Rothman <peter.rothman@avensia.com>
2023-04-21 11:17:01 +0200

    Change error message

--- MvcApp/Controllers/ManageController.cs
+++ MvcApp/Controllers/ManageController.cs
@@ -58,7 +58,7 @@ namespace MvcApp.Controllers
                 message == ManageMessageId.ChangePasswordSuccess ? "Your password has been changed."
                 : message == ManageMessageId.SetPasswordSuccess ? "Your password has been set."
                 : message == ManageMessageId.SetTwoFactorSuccess ? "Your two-factor authentication provider has been set."
-                : message == ManageMessageId.Error ? "An error has occurred."
+                : message == ManageMessageId.Error ? "An awesome error has occurred."
                 : message == ManageMessageId.AddPhoneSuccess ? "Your phone number was added."
                 : message == ManageMessageId.RemovePhoneSuccess ? "Your phone number was removed."
                 : "";
